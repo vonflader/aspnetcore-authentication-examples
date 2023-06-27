60d25f23711b2b0f2300bf43aa0960dd4fd9950a
Szymon Flader <szymon@futur.net.pl>
2023-06-27 14:14:22 +0200

    Added swagger authentication for web api filter auth.

--- AspNetCoreAuthExamples/ApiKeyFilter/WebAppApiKeyFilter/Program.cs
+++ AspNetCoreAuthExamples/ApiKeyFilter/WebAppApiKeyFilter/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using WebAppApiKeyFilter.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,34 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(x => x.Filters.Add<ApiKeyAuthFilter>());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Description = "API Key needed for authentication",
+        Type = SecuritySchemeType.ApiKey,
+        Name = AuthConstants.ApiKeyHeaderName,
+        In = ParameterLocation.Header,
+        Scheme = "ApiKeyScheme"
+    });
+
+    var scheme = new OpenApiSecurityScheme
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header
+    };
+
+    var requirement = new OpenApiSecurityRequirement
+    {
+        { scheme, new List<string>() }
+    };
+
+    options.AddSecurityRequirement(requirement);
+});
 
 var app = builder.Build();
 
