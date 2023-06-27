7fb50e8a9d803471b31020932cc4576054504d00
Szymon Flader <szymon@futur.net.pl>
2023-06-27 14:09:08 +0200

    Added Filter api key authentication using ApiKeys stored in appsettings

---
+++ AspNetCoreAuthExamples/ApiKeyFilter/WebAppApiKeyFilter/Authentication/ApiKeyAuthFilter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebAppApiKeyFilter.Authentication
+{
+    public class ApiKeyAuthFilter : IAuthorizationFilter
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApiKeyAuthFilter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if(!context.HttpContext.Request.Headers.TryGetValue(AuthConstants.ApiKeyHeaderName, out var extractedApiKey))
+            {
+                context.Result = new UnauthorizedObjectResult("Missing API key");
+                return;
+            }
+
+            var apiKeys = _configuration.GetSection(AuthConstants.ApiKeySectionName)
+                                        .Get<List<ApiKeyModel>>() ?? 
+                                        new List<ApiKeyModel>();
+            
+            var validApiKeys = apiKeys.Select(x => x.Key).ToList();
+
+            if (validApiKeys.Contains(extractedApiKey!))
+                return;
+
+            context.Result = new UnauthorizedObjectResult("Invalid API key");
+        }
+    }
+}
---
+++ AspNetCoreAuthExamples/ApiKeyFilter/WebAppApiKeyFilter/Authentication/AuthConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAppApiKeyFilter.Authentication
+{
+    public static class AuthConstants
+    {
+        public const string ApiKeySectionName = "Authentication:ApiKeys";
+        public const string ApiKeyHeaderName = "x-api-key";
+    }
+}
